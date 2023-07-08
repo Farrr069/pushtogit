793b107d5c63f06435cbd6c66c36d7d671d5f5cc
Farrr069 <maulanafarell56@gmail.com>
2023-07-08 19:35:24 +0700

    ppp

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("rrereer");
         System.out.println("ghgh");
+        System.out.println("fgfgfg");
     }
 }
